c9e009ec2bba0a093c2bef0d589f02f5b0592049
Patrick Mooney <pmooney@pfmooney.com>
2022-04-07 12:55:24 -0500

    Allow use of usdt probes with stable rust

--- tests/test-slog-dtrace.rs
+++ tests/test-slog-dtrace.rs
@@ -1,20 +1,21 @@
 //! Integration test for the `slog-dtrace` crate.
 
 // Copyright 2021 Oxide Computer Company
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(asm)]
+#![cfg_attr(not(usdt_stable_asm), feature(asm))]
+#![cfg_attr(all(target_os = "macos", not(usdt_stable_asm_sym)), feature(asm_sym))]
 
 fn main() {}
 
